545a0ba9b22a575ef00ed8f1df3fc6a7d3964df4
Yasen Zhao <xiaoqizhao@outlook.com>
2020-11-16 19:11:23 -0500

    update

--- learning_java/02_a-first-application/HelloJava2.java
+++ learning_java/02_a-first-application/HelloJava2.java
@@ -6,7 +6,13 @@ import javax.swing.*;
 public class HelloJava2 {
     public static void main(String[] args) {
         JFrame frame = new JFrame( "Hello Java 2");
+
         frame.add( new HelloComponent2( "Hello World, Nice Day"));
+
+        // above line equals to below two lines:
+        // HelloComponent2 newObject = new HelloComponent2("Hello World, Nice Day"));
+        // frame.add( newObject );
+
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setSize( 500, 500 );
         frame.setVisible( true );
