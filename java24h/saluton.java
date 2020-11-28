1ff3cc907c7c7e02e527390ae8989559bd782892
Yasen <xiaoqizhao@outlook.com>
2020-11-28 07:49:24 -0500

    learn java24h

---
+++ java24h/saluton.java
@@ -0,0 +1,8 @@
+public class saluton {
+    public static void main(String[] arguments) {
+        System.out.println("hello world");
+        String greeting = "Saluton mondo!";
+        System.out.println(greeting);
+    }
+    
+}
