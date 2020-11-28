b0cbf5c070cd8be78c4cba8c5d20f48e58778033
Yasen <xiaoqizhao@outlook.com>
2020-11-28 18:58:37 -0500

    update

--- java24h/CompareString.java
+++ java24h/CompareString.java
@@ -3,6 +3,9 @@ public class CompareString {
         String favorite = "chainsaw";
         String guess = "pool cue";
         System.out.println("Is Fin's favorite weapon is " + guess + " ?");
+        System.out.println("to Upper Case: " + favorite.toUpperCase());
         System.out.println("Answer: " + favorite.equals(guess));
+        System.out.println("Fin's favorite is " + favorite.length() + " characters");
+
     }
 }
