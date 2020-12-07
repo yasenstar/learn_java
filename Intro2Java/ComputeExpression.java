badd45151292c78996e161eb0bde10fed288514b
Yasen <xiaoqizhao@outlook.com>
2020-12-07 10:46:07 -0500

    learn

---
+++ Intro2Java/ComputeExpression.java
@@ -0,0 +1,9 @@
+public class ComputeExpression {
+    public static void main(String[] args) {
+        double a = (10.5 + 2.0 * 3.0);
+        double b = (45.0 - 3.5);
+        System.out.println(a/b);
+        System.out.print("3.5 * 4 / 2 - 2.5 = ");
+        System.out.println(3.5 * 4 / 2 - 2.5);
+    }
+}
---
+++ Intro2Java/WelcomeJava.java
@@ -0,0 +1,6 @@
+public class WelcomeJava {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Programming is fun!");
+    }
+}
