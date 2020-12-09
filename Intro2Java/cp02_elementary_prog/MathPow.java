5f66bdd8ace8d47f116fe55c19411f47e3fc85fd
Yasen Zhao <xiaoqizhao@outlook.com>
2020-12-09 16:17:24 -0500

    update

---
+++ Intro2Java/cp02_elementary_prog/MathPow.java
@@ -0,0 +1,10 @@
+//package Intro2Java.cp02_elementary_prog;
+
+//import java.Math.*; //?
+
+public class MathPow {
+    public static void main(String[] args){
+        // System.out.println(Math.pow(2,3));
+        System.out.println(Math.pow(4,0.5));
+    }
+}
